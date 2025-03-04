8eef853d2a9bb52cde8298e0638c270a4f7f95b9
Rianderson Correa <ricorrea59@gmail.com>
2025-03-04 10:38:56 -0300

    first commit

---
+++ administrativo/Cliente.java
@@ -0,0 +1,28 @@
+package administrativo;
+
+public class Cliente {
+    private String nome;
+    private int cpf;
+    private int telefone;
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getCpf() {
+        return cpf;
+    }
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+    public int getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    
+}
